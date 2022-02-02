71f1a5a2bcf51f9747b4f2d49d53f024b25241a3
Danilo de Sousa <danspaulo@gmail.com>
2022-02-02 19:56:39 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Danilo de Sousa on 02/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
